7a69c3aa7328f2e2e987cfb8b7d4ab03f01184b2
Honeyoatmeal <toitui64@gmail.com>
2023-06-19 14:18:03 +0900

    item, cart sql 수정

--- hospital/src/main/webapp/sql/Item.sql
+++ hospital/src/main/webapp/sql/Item.sql
@@ -1,1 +1,27 @@
-#item
+--상품
+create table item(
+ item_num number,
+ item_name varchar2(50) not null,
+ item_price number(20) not null,
+ item_quantity number(10) not null,
+ item_photo varchar2(34) not null,
+ item_detail clob not null,
+ item_status number(1) not null,
+ constraint item_pk primary key (item_num)
+);
+
+create sequence item_seq;
+
+
+--장바구니
+create table cart(
+ cart_num number not null,
+ mem_num number not null,
+ item_num number not null,
+ item_count number not null,
+ constraint cart_pk primary key (cart_num),
+ constraint cart_fk foreign key (mem_num) references member (mem_num),
+ constraint cart_fk2 foreign key (item_num) references item (item_num)
+);
+
+create sequence cart_seq;
