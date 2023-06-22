f000c05c75356eb46eb64dab8bd0dde9d874236a
hyein <hyein@LAPTOP-0TBA6GUT>
2023-06-22 16:00:05 +0900

    SelectAction test

---
+++ hospital/src/main/java/kr/reservation/action/SelectAction.java
@@ -0,0 +1,15 @@
+package kr.reservation.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+             
+public class SelectAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		return "/WEB-INF/views/reservation/select.jsp";
+	}
+} 
